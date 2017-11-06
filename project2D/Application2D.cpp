8e5f6da06e1cd2b5f416e24749f6f7d7fb8dd24f
Zachary Briggs <zacharybriggs1@hotmail.com>
2017-11-06 17:04:03 -0600

    Made vector library and messed with 2D graphics

--- project2D/Application2D.cpp
+++ project2D/Application2D.cpp
@@ -12,23 +12,25 @@ Application2D::~Application2D() {
 }
 
 bool Application2D::startup() {
-	
+
 	m_2dRenderer = new aie::Renderer2D();
 
 	m_texture = new aie::Texture("./textures/numbered_grid.tga");
 	m_shipTexture = new aie::Texture("./textures/ship.png");
 
 	m_font = new aie::Font("./font/consolas.ttf", 32);
-	
+
 	m_cameraX = 0;
 	m_cameraY = 0;
 	m_timer = 0;
+	nPx = 640;
+	nPy = 360;
 
 	return true;
 }
 
 void Application2D::shutdown() {
-	
+
 	delete m_font;
 	delete m_texture;
 	delete m_shipTexture;
@@ -38,7 +40,6 @@ void Application2D::shutdown() {
 void Application2D::update(float deltaTime) {
 
 	m_timer += deltaTime;
-
 	// input example
 	aie::Input* input = aie::Input::getInstance();
 
@@ -55,13 +56,33 @@ void Application2D::update(float deltaTime) {
 	if (input->isKeyDown(aie::INPUT_KEY_RIGHT))
 		m_cameraX += 500.0f * deltaTime;
 
+	if (input->isKeyDown(aie::INPUT_KEY_W))
+	{
+		nPy += 500.0f * deltaTime;
+		m_cameraY += 500.0f * deltaTime;
+	}
+	if (input->isKeyDown(aie::INPUT_KEY_S))
+	{
+		nPy -= 500.0f * deltaTime;
+		m_cameraY -= 500.0f * deltaTime;
+	}
+	if (input->isKeyDown(aie::INPUT_KEY_A))
+	{
+		nPx -= 500.0f * deltaTime;
+		m_cameraX -= 500.0f * deltaTime;
+	}
+	if (input->isKeyDown(aie::INPUT_KEY_D))
+	{
+		nPx += 500.0f * deltaTime;
+		m_cameraX += 500.0f * deltaTime;
+	}
+
 	// exit the application
 	if (input->isKeyDown(aie::INPUT_KEY_ESCAPE))
 		quit();
 }
 
 void Application2D::draw() {
-
 	// wipe the screen to the background colour
 	clearScreen();
 
@@ -71,29 +92,31 @@ void Application2D::draw() {
 	// begin drawing sprites
 	m_2dRenderer->begin();
 
-	// demonstrate animation
-	m_2dRenderer->setUVRect(int(m_timer) % 8 / 8.0f, 0, 1.f / 8, 1.f / 8);
-	m_2dRenderer->drawSprite(m_texture, 200, 200, 100, 100);
-
-	// demonstrate spinning sprite
-	m_2dRenderer->setUVRect(0,0,1,1);
-	m_2dRenderer->drawSprite(m_shipTexture, 600, 400, 0, 0, m_timer, 1);
-
-	// draw a thin line
-	m_2dRenderer->drawLine(300, 300, 600, 400, 2, 1);
-
-	// draw a moving purple circle
-	m_2dRenderer->setRenderColour(1, 0, 1, 1);
-	m_2dRenderer->drawCircle(sin(m_timer) * 100 + 600, 150, 50);
-
-	// draw a rotating red box
 	m_2dRenderer->setRenderColour(1, 0, 0, 1);
-	m_2dRenderer->drawBox(600, 500, 60, 20, m_timer);
+	m_2dRenderer->drawBox(nPx, nPy, 60, 60);
+	//// demonstrate animation
+	//m_2dRenderer->setUVRect(int(m_timer) % 8 / 8.0f, 0, 1.f / 8, 1.f / 8);
+	//m_2dRenderer->drawSprite(m_texture, 200, 200, 100, 100);
 
-	// draw a slightly rotated sprite with no texture, coloured yellow
-	m_2dRenderer->setRenderColour(1, 1, 0, 1);
-	m_2dRenderer->drawSprite(nullptr, 400, 400, 50, 50, 3.14159f * 0.25f, 1);
-	
+	//// demonstrate spinning sprite
+	//m_2dRenderer->setUVRect(0,0,1,1);
+	//m_2dRenderer->drawSprite(m_shipTexture, 600, 400, 0, 0, m_timer, 1);
+
+	//// draw a thin line
+	//m_2dRenderer->drawLine(300, 300, 600, 400, 2, 1);
+
+	//// draw a moving purple circle
+	//m_2dRenderer->setRenderColour(1, 0, 1, 1);
+	//m_2dRenderer->drawCircle(sin(m_timer) * 100 + 600, 150, 50);
+
+	//// draw a rotating red box
+	//m_2dRenderer->setRenderColour(1, 0, 0, 1);
+	//m_2dRenderer->drawBox(600, 500, 60, 20, m_timer);
+
+	//// draw a slightly rotated sprite with no texture, coloured yellow
+	//m_2dRenderer->setRenderColour(1, 1, 0, 1);
+	//m_2dRenderer->drawSprite(nullptr, 400, 400, 50, 50, 3.14159f * 0.25f, 1);
+	//
 	// output some text, uses the last used colour
 	char fps[32];
 	sprintf_s(fps, 32, "FPS: %i", getFPS());
--- project2D/main.cpp
+++ project2D/main.cpp
@@ -1,7 +1,10 @@
 #include "Application2D.h"
+#include <Vector2.h>
+#include <Vector3.h>
+#include <Vector4.h>
 
 int main() {
-	
+
 	// allocation
 	auto app = new Application2D();
 
